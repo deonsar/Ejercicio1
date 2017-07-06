312f08ed0628cbbb35d91f258e6d675d53ec630c
usuario <usuario@L-1706038>
2017-07-06 18:18:11 +0200

    Cambios modificados falta configurar la ejecucion

--- Ejercicio1/Class1.cs
+++ Ejercicio1/Class1.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace Ejercicio1
 {
-    public class Class1
+    public class datos
     {
+
+
+        public String text
+        {
+            set;
+            get;
+        }
+        public int num1
+        {
+            set;
+            get;
+        }
+
+        public int num2
+        {
+            set;
+            get;
+        }
     }
+
+    public class metodos : datos
+    {
+
+
+        datos datos1 = new datos();
+        public String mensaje()
+        {
+            datos1.text = "nuevo texto";
+            datos1.num1 = 1;
+            datos1.num2 = 2;
+            
+            return "Los datos son: "+datos1.text+" "+datos1.num1+" "+datos1.num2;
+        }
+    }
+
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejercicio1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private metodos sut ;
+
+        [TestInitialize]
+        public void UnitTestProject()
+        {
+            sut = new metodos();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            String texto = sut.mensaje();
+            Assert.AreSame("Los datos son: nuevo texto 1 2",texto);
+
+        }
+    }
+}
